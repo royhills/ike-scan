2d5e1ce42272472cd7898b9c96f6d08736ff96d8
Roy Hills <Roy.Hills@nta-monitor.com>
2002-09-16 14:53:51 +0000

    Increased DEFAULT_INTERVAL from 50 to 75ms to reduce bandwidth to 4.6KB/sec
    git-svn-id: svn+ssh://svn.nta-monitor.com/trunk/opensource/ike-scan@500 062a1500-4a13-0410-a63b-ee65f32af78f

--- ike-scan.h
+++ ike-scan.h
@@ -12,7 +12,7 @@
 #define DEFAULT_BACKOFF_FACTOR 1.5	/* Default timout backoff factor */
 #define DEFAULT_RETRY 3			/* Default number of retries */
 #define DEFAULT_TIMEOUT 500		/* Default per-host timeout in ms */
-#define DEFAULT_INTERVAL 50		/* Default delay between packets (ms) */
+#define DEFAULT_INTERVAL 75		/* Default delay between packets (ms) */
 #define DEFAULT_SOURCE_PORT 500		/* Default UDP source port */
 #define DEFAULT_DEST_PORT 500		/* Default UDP destination port */
 #define DEFAULT_LIFETIME 28800		/* Default lifetime in seconds */
