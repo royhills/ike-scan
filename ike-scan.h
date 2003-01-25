426c180903629b1125998566f124d59100cf40e1
Roy Hills <Roy.Hills@nta-monitor.com>
2003-01-25 12:18:14 +0000

    Changed host_entry element "n" from int to unsigned.
    git-svn-id: svn+ssh://svn.nta-monitor.com/trunk/opensource/ike-scan@643 062a1500-4a13-0410-a63b-ee65f32af78f

--- ike-scan.h
+++ ike-scan.h
@@ -94,7 +94,7 @@
 struct host_entry {
    struct host_entry *prev;	/* Previous pointer */
    struct host_entry *next;	/* Next pointer */
-   int n;			/* Ordinal number for this entry */
+   unsigned n;			/* Ordinal number for this entry */
    struct in_addr addr;		/* Host IP address */
    u_char live;			/* Set when awaiting response */
    struct timeval last_send_time; /* Time when last packet sent to this addr */
