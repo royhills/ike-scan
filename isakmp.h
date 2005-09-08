4e9595fc06f0cc5149504eb5fb54af08fe6237e7
Roy Hills <Roy.Hills@nta-monitor.com>
2005-09-08 15:39:03 +0000

    Added OpenSSL exception to copyright text.
    git-svn-id: svn+ssh://svn.nta-monitor.com/trunk/opensource/ike-scan@4186 062a1500-4a13-0410-a63b-ee65f32af78f

--- isakmp.h
+++ isakmp.h
@@ -16,6 +16,17 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  *
+ * In addition, as a special exception, the copyright holders give
+ * permission to link the code of portions of this program with the
+ * OpenSSL library, and distribute linked combinations including the two.
+ *
+ * You must obey the GNU General Public License in all respects
+ * for all of the code used other than OpenSSL.  If you modify
+ * file(s) with this exception, you may extend this exception to your
+ * version of the file(s), but you are not obligated to do so.  If you
+ * do not wish to do so, delete this exception statement from your
+ * version.
+ *
  * If this license is unacceptable to you, I may be willing to negotiate
  * alternative licenses (contact ike-scan@nta-monitor.com).
  *
