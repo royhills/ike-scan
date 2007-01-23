75607fd9cd07efd4b7d2d3c42a662da943d6a480
Roy Hills <Roy.Hills@nta-monitor.com>
2007-01-23 14:41:30 +0000

    Preparations for ike-scan 1.9 release.
    git-svn-id: svn+ssh://svn.nta-monitor.com/trunk/opensource/ike-scan@9926 062a1500-4a13-0410-a63b-ee65f32af78f

--- make-win32-zipfile.sh
+++ make-win32-zipfile.sh
@@ -4,18 +4,18 @@
 # make-win32-zipfile.sh -- Create Win32 distribution zip file
 #
 # This shell script creates the ike-scan Win32 binary zip file
-# ike-scan-win32-<ver>.zip.  I use it to create the ike-scan windows binary
+# ike-scan-win32-<ver>.zip. It is used it to create the ike-scan windows binary
 # package for each new version.  Most users won't need to use this script, and
 # it can be omitted from binary packages without any loss of functionallity.
 #
 # It must be run under Cygwin after ike-scan has been configured, built and
-# tested.  It will not work on Unix or Linux systems.
+# tested. It will not work on Unix or Linux systems.
 #
 # The resulting zipfile can be used on any Win32 system.
 #
 FILES="udp-backoff-fingerprinting-paper.txt AUTHORS ChangeLog COPYING ike-backoff-patterns ike-scan.exe ike-vendor-ids NEWS psk-crack.exe psk-crack-dictionary README README-WIN32 TODO /cygdrive/c/cygwin/bin/cygwin1.dll"
 
-ZIPFILE="ike-scan-win32-1.7.9.zip"
+ZIPFILE="ike-scan-win32-1.9.zip"
 OPTS="-j -9"
 #
 zip $OPTS $ZIPFILE $FILES
