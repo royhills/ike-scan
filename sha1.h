32b86d31f66cc9adf04c2616b6e9eacad5200892
Roy Hills <Roy.Hills@nta-monitor.com>
2007-06-15 08:29:47 +0000

    Added GPL License to some source files that were missing it.
    git-svn-id: svn+ssh://svn.nta-monitor.com/trunk/opensource/ike-scan@10908 062a1500-4a13-0410-a63b-ee65f32af78f

--- sha1.h
+++ sha1.h
@@ -1,4 +1,39 @@
-/* $Id$
+/*
+ * The IKE Scanner (ike-scan) is Copyright (C) 2003-2007 Roy Hills,
+ * NTA Monitor Ltd.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ * In addition, as a special exception, the copyright holders give
+ * permission to link the code of portions of this program with the
+ * OpenSSL library, and distribute linked combinations including the two.
+ *
+ * You must obey the GNU General Public License in all respects
+ * for all of the code used other than OpenSSL.  If you modify
+ * file(s) with this exception, you may extend this exception to your
+ * version of the file(s), but you are not obligated to do so.  If you
+ * do not wish to do so, delete this exception statement from your
+ * version.
+ *
+ * If this license is unacceptable to you, I may be willing to negotiate
+ * alternative licenses (contact ike-scan@nta-monitor.com).
+ *
+ * You are encouraged to send comments, improvements or suggestions to
+ * me at ike-scan@nta-monitor.com.
+ *
+ * $Id$
  *
  * C Header file for Steve Reid's SHA1 implementation.
  *
